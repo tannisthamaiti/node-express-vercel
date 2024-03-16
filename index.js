b0056ff9d83b335c1264644249a528176ab67ac3
tannisthamaiti <37048897+tannisthamaiti@users.noreply.github.com>
2024-03-16 10:27:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,30 @@ const fs = require('fs');
 const path = require('path');
 
 const dataFilePath = path.join(__dirname, 'data', 'data.json');
+
+app.post('/save-data', (req, res) => {
+  const newData = req.body;
+  fs.readFile(dataFilePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading file:', err);
+      return res.status(500).json({ message: 'Error saving data.' });
+    }
+    
+    const existingData = JSON.parse(data);
+   
+    //console.log(existingData);
+    existingData.push(newData);
+
+    fs.writeFile(dataFilePath, JSON.stringify(existingData, null, 2), (err) => {
+      if (err) {
+        console.error('Error writing to file:', err);
+        return res.status(500).json({ message: 'Error saving data.' });
+      }
+      console.log('Data saved to file successfully.');
+      res.json({ message: 'Data saved successfully.' });
+    });
+  });
+});
   
 // A simple get greet method 
 app.get("/greet", (req, res) => { 
