94092e02d6dc7cb7a8859e3f1c0a18ccd3057a42
tannisthamaiti <37048897+tannisthamaiti@users.noreply.github.com>
2024-03-16 07:59:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-// Import packages
-const express = require("express");
-const home = require("./routes/home");
+const http = require('http');
+const fs = require('fs');
+const port =3000;
 
-// Middlewares
-const app = express();
-app.use(express.json());
+const server = http.createServer(function(req,res){
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    fs.readFile('index.html',function(error,data){
+        if (error){
+            
+            res.writeHead(404)
+            res.write("Error: File not found")
+        }else{
+            res.write(data)
+        }
+        res.end();
+    })
 
-// Routes
-app.use("/home", home);
+})
 
-// connection
-const port = process.env.PORT || 9001;
-app.listen(port, () => console.log(`Listening to port ${port}`));
+server.listen(port,function(error){
+    if (error){
+        console.log("Something went wrong", error)
+    }
+    else
+    console.log("Server is listening on port "+ port)
+})
