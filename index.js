1e2677275dc555b5d94dae0b892fca2f317dbb2c
tannisthamaiti <tannistha.iitkgp@gmail.com>
2024-03-16 08:33:48 -0400

    vercel

--- index.js
+++ index.js
@@ -1,45 +1,14 @@
-// server.js
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-
-const app = express();
-const port = 3000;
-
-app.use(express.json());
-//app.use(express.static('public'));
-
-const dataFilePath = path.join(__dirname, 'data', 'data.json');
-
-app.post('/save-data', (req, res) => {
-  const newData = req.body;
+// server.js 
   
-  fs.readFile(dataFilePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error('Error reading file:', err);
-      return res.status(500).json({ message: 'Error saving data.' });
-    }
-    
-    const existingData = JSON.parse(data);
-   
-    //console.log(existingData);
-    existingData.push(newData);
-
-    fs.writeFile(dataFilePath, JSON.stringify(existingData, null, 2), (err) => {
-      if (err) {
-        console.error('Error writing to file:', err);
-        return res.status(500).json({ message: 'Error saving data.' });
-      }
-      console.log('Data saved to file successfully.');
-      res.json({ message: 'Data saved successfully.' });
-    });
-  });
-});
-
-
-
-app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}`);
-});
-// const port = process.env.PORT || 9001;
-// app.listen(port, () => console.log(`Listening to port ${port}`));
+const express = require("express"); 
+const app = express(); 
+  
+// A simple get greet method 
+app.get("/greet", (req, res) => { 
+    // get the passed query 
+    const { name } = req.query; 
+    res.send({ msg: `Welcome ${name}!` }); 
+}); 
+  
+// export the app for vercel serverless functions 
+module.exports = app;
