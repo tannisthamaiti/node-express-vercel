b8bf8aaeca04ff3c1d710471a2981a0f4eefad58
tannisthamaiti <37048897+tannisthamaiti@users.noreply.github.com>
2024-03-16 10:17:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express"); 
 const app = express(); 
+app.use(express.static(__dirname + "/public"));
   
 // A simple get greet method 
 app.get("/greet", (req, res) => { 
@@ -9,4 +10,5 @@ app.get("/greet", (req, res) => {
 }); 
   
 // export the app for vercel serverless functions 
+
 module.exports = app;
