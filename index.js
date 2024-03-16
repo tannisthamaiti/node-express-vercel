3675ca7686f0f94464c96ba95b9f68711a6ccb9e
tannisthamaiti <37048897+tannisthamaiti@users.noreply.github.com>
2024-03-16 08:05:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port =3000;
 
 const server = http.createServer(function(req,res){
     res.writeHead(200, {'Content-Type': 'text/html'})
-    fs.readFile('.index.html',function(error,data){
+    fs.readFile('index.html',function(error,data){
         if (error){
             
             res.writeHead(404)
